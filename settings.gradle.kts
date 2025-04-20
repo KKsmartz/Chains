5501cfc97a88e2efb48749ca934756813c4e7f06
kunka <kunka@micous.com>
2025-04-20 11:03:15 +0800

    feat: 调整项目结构

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "smartz-chains"
+include(":smartz-player:app")
+include(":smartz-im:app")
+
+include(":smartz-map")
